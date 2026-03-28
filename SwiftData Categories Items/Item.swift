82ef60971aa160839d60e998a4f285c86c7e71f2
Remo Prozzillo <55060292+RemoPro@users.noreply.github.com>
2026-03-28 15:06:44 +0100

    Initial Commit

---
+++ SwiftData Categories Items/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftData Categories Items
+//
+//  Created by Remo Prozzillo on 28.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
